6ba037492516cb2d3c4468a54cc93a95acc5e670
Matthew <matthewproskils@gmail.com>
2021-06-24 07:47:37 -0700

    Removed Files

--- js/globaldeclarations.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=globaldeclarations.js.map
--- js/index.js
+++
@@ -1,4 +0,0 @@
-import levelOne from "./levels/one.js";
-const levelInstance = new levelOne();
-levelInstance.init();
-//# sourceMappingURL=index.js.map
